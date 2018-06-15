aaac60fcb897bf1fa886b0229cd778a37c9ecd67
danielpinto01 <daniel.pinto01@uptc.edu.co>
2018-06-14 21:42:31 -0500

    Se creo la clase Manager

--- src/models/Manager.java
+++ src/models/Manager.java
@@ -1,5 +1,24 @@
 package models;
 
-public class Manager {
+import java.util.ArrayList;
 
+public class Manager {
+	
+	private ArrayList<Player> players;
+	
+	public Manager() {
+		players = new ArrayList<>();
+	}
+	
+	public static Player createPlayer(String namePlayer, String characterPlayer) {
+		return new Player(namePlayer, characterPlayer);
+	}
+	
+	public void addPlayerToList(Player player) {
+		players.add(player);
+	}
+
+	public ArrayList<Player> getPlayers() {
+		return players;
+	}
 }
